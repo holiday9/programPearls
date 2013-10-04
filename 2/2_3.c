1489124428383c1b2d91e0335539f4b7fe1a1799
htyuan <htyuan80@gmail.com>
2013-10-04 11:35:52 +0800

    编写代码完成，但运行出错。在这里保存是为了以后可以练习使用gdb调试

--- 2/2_3.c
+++ 2/2_3.c
@@ -15,5 +15,52 @@
  *
  * =====================================================================================
  */
+#include<stdio.h>
 
+int gcd(int i, int j) {
+	while(i != j) {
+		if (i > j) {
+			i -= j;	
+		} else {
+			j -= i;
+		}
+	}
+	
+	return i;
+}
 
+void rotate_str(char * x, int rotdist, int n) {
+	int i;
+	for (i = 0;i < gcd(rotdist, n); i++) {
+		int t = x[i];
+		int j = i;
+		while (1) {
+			int k = j + rotdist;
+			if (k >= n) {
+				k -= n;
+			}
+			if (k == i) 
+				break;
+			x[j] = x[k];
+			j = k;	
+		}
+		x[j] = t;	
+	}
+}
+
+void print_str(char a[]) {
+	int i = 0;
+	printf("%d\n", sizeof(a));
+	for (i = 0;i < 7;i++) {
+		putchar(a[i]);
+	}
+	putchar('\n');
+}
+int main() {
+	char x[] = {'a','b','c','1','3','5','7'};
+	print_str(x);
+	rotate_str(x, 7, 3);
+	print_str(x);
+
+	return 0;
+}
