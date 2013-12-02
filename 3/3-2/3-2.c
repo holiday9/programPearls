032e4fb46d854d64aeb7d3655d042d31334cc801
htyuan <htyuan80@gmail.com>
2013-12-02 12:08:16 +0800

    3-2定义清楚了问题。

---
+++ 3/3-2/3-2.c
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  3-2.c
+ *
+ *    Description:  source code for question 3-2
+ *
+ *        Version:  1.0
+ *        Created:  2013年11月21日 19时31分56秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  htyuan (htyuan), htyuan80@gmail.com
+ *        Company:  aibang
+ *
+ * =====================================================================================
+ */
+
+
